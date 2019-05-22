7b5c1c9d910ef615d745be736f27ffc2def4b6e0
Irmina Stepien <irm.stepien@gmail.com>
2019-05-22 11:35:21 +0200

    bug fixed: change of color disabled

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -9,54 +9,54 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int isUserActive = 0;
+
 
     int [] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2 };
     int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8},
                                 {0,3,6}, {1,4,7}, {2,5,8},
                                 {0,4,8}, {2,4,6}};
+    int isUserActive = 0;
 
     public void dropIn (View view){
         ImageView counter = (ImageView) view;
-        counter.setTranslationX(-2000);
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        gameState[tappedCounter] = isUserActive;
+        if(gameState[tappedCounter]==2) {
+
+            gameState[tappedCounter] = isUserActive;
+            counter.setTranslationX(-2000);
 
 
-        if(isUserActive==0) {
-            counter.setImageResource(R.drawable.green);
-            isUserActive=1;
+            if (isUserActive == 0) {
+                counter.setImageResource(R.drawable.green);
+                isUserActive = 1;
 
-        }
-        else{
-            counter.setImageResource(R.drawable.red);
-            isUserActive=0;
-        }
-
+            } else {
+                counter.setImageResource(R.drawable.red);
+                isUserActive = 0;
+            }
 
 
-        counter.animate().translationXBy(2000).setDuration(500);
+            counter.animate().translationXBy(2000).setDuration(500);
 
-        for(int[] winningPosition : winningPositions){
-                 if(gameState[winningPosition[0]]==gameState[winningPosition[1]] &&
-                    gameState[winningPosition[1]]==gameState[winningPosition[2] ]&&
-                    gameState[winningPosition[2]]!=2){
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[2]] != 2) {
 
-                     String winner ="";
+                    String winner = "";
 
-                     if(isUserActive==0){
-                         winner="red";
-                     }
-                     else{
-                         winner="green";
-                     }
+                    if (isUserActive == 0) {
+                        winner = "red";
+                    } else {
+                        winner = "green";
+                    }
 
-                     Toast.makeText(this, winner+" has won", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, winner + " has won", Toast.LENGTH_SHORT).show();
+                }
             }
         }
-
     }
 
     @Override
