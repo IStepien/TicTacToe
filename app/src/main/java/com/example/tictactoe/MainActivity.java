2af0a738783d128fcd0a5aeef67551445d3d6604
Irmina Stepien <irm.stepien@gmail.com>
2019-05-22 09:09:11 +0200

    dropIn method added

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,8 +2,19 @@ package com.example.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn (View view){
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationX(-2000);
+        counter.setImageResource(R.drawable.green);
+        counter.animate().translationXBy(2000).setDuration(500);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
